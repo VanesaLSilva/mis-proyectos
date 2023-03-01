37d0b2b04086cff8c9d9b601038c4e99ea20e8de
VanesaLSilva <loreleysilva@hotmail.com>
2023-03-01 01:59:43 -0300

    Trabajo concluido

--- proyectos/script.js
+++ proyectos/script.js
@@ -28,7 +28,7 @@
   $form.addEventListener("submit", (e) => {
     e.preventDefault();
     $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/aguijavier58@gmail.com", {
+    fetch("https://formsubmit.co/ajax/loreleysilva@hotmail.com", {
       method: "POST",
       body: new FormData(e.target),
     })
